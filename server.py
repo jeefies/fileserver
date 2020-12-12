7a36a8c2280784f23267f4ca27d10d2f2c6fc173
jeefies <jeefy163@163.com>
2020-12-12 13:57:20 +0800

    mute 5001

--- server.py
+++ server.py
@@ -7,29 +7,34 @@ from wtforms import FileField, SubmitField, StringField
 from wtforms.validators import DataRequired
 from multiprocessing import Process
 
+
 class FileForm(FlaskForm):
-    file = FileField('Please choose the file you\'d like to upload', \
-            validators=[DataRequired(message='Your must give in a file')])
+    file = FileField('Please choose the file you\'d like to upload',
+                     validators=[DataRequired(message='Your must give in a file')])
     submit = SubmitField('Submit')
+
 
 path = os.getcwd()
 base = os.path.dirname(os.path.abspath(__file__))
 tempf = os.path.join(base, 'temps')
 
-app = Flask(__name__, template_folder = tempf, static_folder=path)
+app = Flask(__name__, template_folder=tempf, static_folder=path)
 app.config['SECRET_KEY'] = 'hard to get a kkey'
 bootstrap = Bootstrap(app)
 
 _j = os.path.join
 p = _j(base, 'static')
-_app = Flask(__name__, static_folder = p)
+_app = Flask(__name__, static_folder=p)
 static = 'http://0.0.0.0:5001/static'
-stcs = [os.path.join(static, n) for n in ['jquery.js', 'bootstrap.js','bootstrap.css', 'favicon.ico']]
+stcs = [os.path.join(static, n) for n in ['jquery.js',
+                                          'bootstrap.js', 'bootstrap.css', 'favicon.ico']]
 del _j, p
+
 
 def render(file, **kwargs):
     kwargs['_sts'] = stcs
     return render_template(file, **kwargs)
+
 
 @app.route('/', methods='GET POST'.split())
 def index():
@@ -40,18 +45,21 @@ def index():
         p = os.path.join(path, name)
         flash('Upload success!')
         data.save(p)
-        form.file = FileField('Please choose the file you\'d like to upload', \
-            validators=[DataRequired(message='Your must give in a file')])
+        form.file = FileField('Please choose the file you\'d like to upload',
+                              validators=[DataRequired(message='Your must give in a file')])
     return render('index.html', form=form)
+
 
 @app.route('/path/<path:stcp>')
 def download(stcp):
     viewp = os.path.join(path, stcp)
     return check(viewp, stcp)
 
+
 @app.route('/path')
 def downloadm():
     return check(path, '')
+
 
 def check(stcp, start):
     dirs = os.listdir(stcp)
@@ -59,19 +67,25 @@ def check(stcp, start):
     ad = d.append
     f = []
     af = f.append
-    isd = os.path.isdir
+    isf = os.path.isfile
     absp = os.path.abspath
+    j = os.path.join
     for i in dirs:
-        a = absp(i)
+        a = absp(j(start, i))
         p = os.path.join(start, i) if start else i
-        if not isd(a):
+        if isf(a):
             af((i, url_for('static', filename=p)))
         else:
             ad((i, url_for('download', stcp=p)))
-    return render('files.html', dirs = d, files = f)
+    return render('files.html', dirs=d, files=f)
+
 
 if __name__ == '__main__':
-    Process(target= _app.run, args = ('0.0.0.0', 5001)).start()
-    import time
-    time.sleep(0.2)
-    Process(target= app.run, args = ('0.0.0.0', 5000)).start()
+    def run_app():
+        import io
+        from contextlib import redirect_stdout, redirect_stderr
+        f = io.StringIO()
+        with redirect_stderr(f), redirect_stdout(f):
+            _app.run('0.0.0.0', 5001)
+    Process(target=run_app).start()
+    Process(target=app.run, args=('0.0.0.0', 5000)).start()
